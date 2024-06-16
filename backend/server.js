891307e3f63c0d8d1e26701392d4ce65e251cdb4
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-16 07:15:27 +0600

    created dynamic port for backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
     res.send("Server is ready")
 })
 
 
-app.listen(5000, () => console.log("The server is running on port 5000"))
+app.listen(5000, () => console.log(`The server is running on port ${PORT}`))
